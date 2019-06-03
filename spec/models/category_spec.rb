3647c72b579e81a107aed3c77ba5cd7909370dc8
Gustavo Chapim <gustavo.chapim@gmail.com>
2019-06-03 14:00:04 +0000

    Testing scope

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,5 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'active scope' do
+    context 'given active category' do
+      let!(:category) { create(:category, active: true) }
+
+      it 'returns active' do
+        expect(Category.active.size).to be_eql(1)
+      end
+    end
+
+    context 'given inactive category' do
+      let!(:category) { create(:category, active: false) }
+
+      it 'returns active' do
+        expect(Category.active.size).to be_eql(0)
+      end
+    end
+  end
 end
